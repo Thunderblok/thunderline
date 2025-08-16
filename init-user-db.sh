b119d2fc979b138b40929bc34914e3a0816f2f89
thunderblok <mo@okoracle.com>
2025-08-15 22:15:55 -0400

    Remove terminalselection file and address related errors in Thunderline application startup

--- init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the thunderline user and database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER thunderline WITH SUPERUSER PASSWORD 'thunderline';
-    ALTER USER thunderline CREATEDB;
-    GRANT ALL PRIVILEGES ON DATABASE thunderline_dev TO thunderline;
-EOSQL
